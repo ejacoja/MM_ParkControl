72627bbdf5f49dfcc2c6219db2e146e9ec92e8c0
Andreas <andreas.mueller27@volkswagen.de>
2021-10-11 15:57:15 +0200

    Added new Map, SpawnPoints

---
+++ Assets/Spawn/SpawnMgr.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMgr : MonoBehaviour
+{
+    public List<Color> CarColors = new List<Color>();
+    public List<CarSpawner> CarSpawnPoints = new List<CarSpawner>();
+    public List<GameObject> CarPrefabs;
+    public float SpawnTime = 5f, ArrowWarnTime = 2f;
+
+    private int colorIndex = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCarsAtRandom());
+    }
+
+    IEnumerator SpawnCarsAtRandom()
+    {
+        while(true)
+        {
+            int curCarIndex = colorIndex % CarColors.Count;
+
+            CarSpawner spawner = CarSpawnPoints[Random.Range(0, CarSpawnPoints.Count)];
+            spawner.ShowOut(CarColors[curCarIndex]);
+
+            yield return new WaitForSeconds(ArrowWarnTime);
+
+            Instantiate<GameObject>(CarPrefabs[curCarIndex], spawner.transform.position, spawner.transform.rotation, null);
+            colorIndex++;
+
+            yield return new WaitForSeconds(0.5f);
+            spawner.ShowNone();
+
+            yield return new WaitForSeconds(SpawnTime);
+        }
+    }
+}
--- Assets/SpawnPoints.cs
+++ Assets/SpawnPoints.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpawnPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
